db12b146731b8e5a893beca321e0207714840eb7
razkafltsyh <muhammad.razka41@ui.ac.id>
2025-09-03 13:38:19 +0700

    Complete tutorial 1: Django MVT implementation

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2406496340',
+        'name': 'Muhammad Razka Faltasyah',
+        'class': 'PBP E'
+    }
+
+    return render(request, "main.html", context)
