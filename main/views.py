e68be3f847f7c7868d3f205949d914e105ad16f1
razkafltsyh <muhammad.razka41@ui.ac.id>
2025-09-10 14:10:03 +0700

    Tutorial 2 selesai

--- main/views.py
+++ main/views.py
@@ -8,9 +8,9 @@ def show_main(request):
     news_list = News.objects.all()
 
     context = {
-        'npm' : '240123456',
-        'name': 'Haru Urara',
-        'class': 'PBP A',
+        'npm' : '2406496340',
+        'name': 'Muhammad Razka Faltasyah',
+        'class': 'PBP E',
         'news_list': news_list
     }
 
